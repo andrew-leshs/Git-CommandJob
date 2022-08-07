c8d5b5b0a08ff548340bd74b835b708e3e5ac2db
Ekaterina Shadrintseva <kateornotkate@gmail.com>
2022-08-07 17:24:56 +0500

    Акция 3 = 2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,10 @@ public class Main {
         int[] prices = {50, 100, 250};
         int[] amount = new int[products.length];
 
+        String[] saleProducts = {"Вино", "Кофе", "Шоколад"};
+        int[] salePrices = {500, 300, 150};
+        int[] saleAmount = new int[salePrices.length];
+
         System.out.println("Список возможных товаров для покупки: ");
         for (int i = 0; i < products.length; i++) {
             System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб/шт");
@@ -18,10 +22,18 @@ public class Main {
         int productCount;
         int sum = 0;
 
-        while (true) {
-            System.out.println("Введите номер продукта и его количество через пробел. Для оформления корзины введите \"end\".");
-            String input = scanner.nextLine();
+        System.out.println("\n" + "Список возможных товаров по акции 3=2: ");
+        for (int j = 0; j < saleProducts.length; j++) {
+            System.out.println((j + 4) + ". " + saleProducts[j] + " " + salePrices[j] + " руб/шт");
+        }
 
+        int saleNumber;
+        int saleCount;
+        int saleTotal = 0;
+
+        while (true) {
+            System.out.println("\n" + "Введите номер продукта и его количество через пробел. Для оформления корзины введите \"end\".");
+            String input = scanner.nextLine();
             if (input.equals("") || input.equals(" ")) {
                 System.out.println("Внимание! Вводить необходимо только числа. Вы же ввели пробел или оставили пустую строку.");
                 continue;
@@ -45,7 +57,7 @@ public class Main {
                 continue;
             }
 
-            if ((productNumber + 1) > products.length) {
+            if ((productNumber + 1) > (products.length + saleProducts.length)) {
                 System.out.println("Внимание! \"НОМЕР ПРОДУКТА\" в списке товаров не найден.");
                 continue;
             }
@@ -60,18 +72,33 @@ public class Main {
                 continue;
             }
 
-            amount[productNumber] += productCount;
-            int sumProducts = productCount * prices[productNumber];
-            sum += sumProducts;
+            if (productNumber < products.length) {
+                amount[productNumber] += productCount;
+                int sumProducts = productCount * prices[productNumber];
+                sum += sumProducts;
+            } else {
+                saleNumber = productNumber - products.length;
+                saleCount = productCount;
+                saleAmount[saleNumber] += saleCount;
+            }
         }
 
         System.out.println("Ваша корзина: ");
         for (int i = 0; i < amount.length; i++) {
             if (amount[i] != 0) {
-                System.out.println(products[i] + " " + amount[i] + " шт " + prices[i] + " руб/шт "
-                        + (amount[i] * prices[i]) + " руб в сумме.");
+                System.out.println(products[i] + " " + amount[i] + " шт " + prices[i] + " руб/шт " + (amount[i] * prices[i]) + " руб в сумме.");
             }
         }
-        System.out.println("Итого: " + sum + " руб.");
+
+        System.out.println("Товары по акции: ");
+        for (int j = 0; j < saleAmount.length; j++) {
+            if (saleAmount[j] != 0) {
+                int saleSum = (saleAmount[j] - (saleAmount[j] / 3)) * salePrices[j];
+                System.out.println(saleProducts[j] + " " + saleAmount[j] + " шт " +
+                        salePrices[j] + " руб/шт " + saleSum + " руб в сумме.");
+                saleTotal += saleSum;
+            }
+        }
+        System.out.println("Итого: " + (sum + saleTotal));
     }
 }
